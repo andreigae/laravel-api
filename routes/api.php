d9cfe88bf2e00c84f4b82afb8473c34f8980d93b
Andrei Gae <andrei@macrofunnels.com>
2023-09-12 01:59:09 +0200

    Creando las Rutas de Recurso para Cada Controlador

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\User\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,8 +17,42 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
-*/
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+*/
+
+
+/**
+ * Buyers
+ */
+
+Route::resource('buyers', BuyerController::class)->only(['index', 'show']);
+
+/**
+ * Categories
+ */
+Route::resource('categories', CategoryController::class)->except(['create', 'edit']);
+
+/**
+ * Products
+ */
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+
+/**
+ * Transactions
+ */
+Route::resource('transactions', TransactionController::class)->only(['index', 'show']);
+
+/**
+ * Sellers
+ */
+Route::resource('sellers', SellerController::class)->only(['index', 'show']);
+
+/**
+ * Users
+ */
+Route::resource('users', UserController::class)->except(['create', 'edit']);
