67b7641c7bac1cd63e3cce1cec3573028775924d
Andrei Gae <andrei@macrofunnels.com>
2023-09-12 03:00:49 +0200

    Creando el Factory de Product

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Product;
+use App\Models\User;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->word,
+            'description' => fake()->paragraph(1),
+            'quantity' => fake()->numberBetween(1, 10),
+            'status' => fake()->randomElement([Product::PRODUCTO_DISPONIBLE, Product::PRODUCTO_NO_DISPONIBLE]),
+            'image' => fake()->randomElement(['1.jpg', '2.jpg', '3.jpg']),
+            // 'seller_id' => User::inRandomOrder()->first()->id,
+            'seller_id' => User::all()->random()->id,
+        ];
+    }
+}
