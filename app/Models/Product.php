a7e563e9182540b85ddef9accb513aac782d2543
Andrei Gae <andrei@macrofunnels.com>
2023-09-12 02:01:54 +0200

    Implementando las Propiedades para Product

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    const PRODUCTO_DISPONIBLE = 'disponible';
+	const PRODUCT_NO_DISPONIBLE = 'no disponible';
+
+    protected $fillable = [
+    	'name',
+    	'description',
+    	'quantity',
+    	'status',
+    	'image',
+    	'seller_id',
+    ];
+
+    public function estaDisponible()
+    {
+    	return $this->status == Product::PRODUCTO_DISPONIBLE;
+    }
+
+
 }
