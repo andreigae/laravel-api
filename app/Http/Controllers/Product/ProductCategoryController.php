ed7e3e19cee48b865111977d6642bf770c9c26c5
Andrei Gae <andrei@macrofunnels.com>
2023-09-14 20:44:19 +0200

    Implementando la Operación Destroy de ProductCategory

--- app/Http/Controllers/Product/ProductCategoryController.php
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -9,11 +9,6 @@ use App\Models\Category;
 
 class ProductCategoryController extends ApiController
 {
-   /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index(Product $product)
     {
         $categories = $product->categories;
@@ -21,13 +16,7 @@ class ProductCategoryController extends ApiController
         return $this->showAll($categories);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
+   
     public function update(Request $request, Product $product, Category $category)
     {
         // sync, attach, syncWithoutDetaching
@@ -40,14 +29,15 @@ class ProductCategoryController extends ApiController
         return $this->showAll($product->categories);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Product  $product
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Product $product)
+   
+    public function destroy(Product $product, Category $category)
     {
-        //
+        if (!$product->categories()->find($category->id)) {
+            return $this->errorResponse('La categoría especificada no es una categoría de este producto', 404);
+        }
+
+        $product->categories()->detach([$category->id]);
+
+        return $this->showAll($product->categories);
     }
 }
