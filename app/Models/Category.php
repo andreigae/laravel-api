f0183f5be9a5ceecb593d8c9fb26a5e59690d2a6
Andrei Gae <andrei@macrofunnels.com>
2023-09-14 07:33:31 +0200

    Removiendo la Tabla Pivote de los Resultados

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -17,6 +17,10 @@ class Category extends Model
     	'description',
     ];
 
+    protected $hidden = [
+        'pivot'
+    ];
+
     // hasMany is used in a One To Many relationship (Un comprador tiene muchas transacciones)
     // while belongsToMany refers to a Many To Many relationship. (Una categoría puede tener muchos productos, y un producto puede tener muchas categorías)
     public function products()
