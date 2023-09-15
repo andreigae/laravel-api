811f39ee5f693b66d1e6631db34ec7da2e486b4f
Andrei Gae <andrei@macrofunnels.com>
2023-09-15 18:50:15 +0200

    Creando un Middleware Personalizado

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use App\Http\Middleware\SignatureMiddleware;
 
 class Kernel extends HttpKernel
 {
@@ -30,6 +31,7 @@ class Kernel extends HttpKernel
      */
     protected $middlewareGroups = [
         'web' => [
+            SignatureMiddleware::class.':X-Application-Name',
             \App\Http\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -39,6 +41,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            SignatureMiddleware::class.':X-Application-Name',
             \App\Http\Middleware\ForceJsonResponse::class,
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
