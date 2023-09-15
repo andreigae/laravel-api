40d3b4849f2b60aa3b081b714538f3cc053fffc6
Andrei Gae <andrei@macrofunnels.com>
2023-09-15 09:56:13 +0200

    Implementando el Mailable de UserMailChanged

--- app/Mail/UserMailChanged.php
+++ app/Mail/UserMailChanged.php
@@ -27,7 +27,7 @@ class UserMailChanged extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'User Mail Changed',
+            subject: 'Por favor confirma tu nuevo correo',
         );
     }
 
@@ -37,7 +37,7 @@ class UserMailChanged extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'view.name',
+            view: 'emails.confirm',
         );
     }
 
